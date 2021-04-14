04dc36a990f0683f5d070c8152a847056b147a69
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-04-14 14:25:55 +0530

    initial commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -23,6 +23,7 @@ export class home extends Component {
         this.setState({ checked: !this.state.checked });
     }
     render() {
+        
         var mytxt = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nisi quasi, possimus enim magnam soluta accusamus beatae temporibus officia error animi dolorum voluptatum iste! Quidem, culpa ipsam! Necessitatibus, voluptatum expedita!"
         if(this.state.checked === true){
             console.log("checked")
