74d0e861b7ac931118f9f326ea6c246a71857bd3
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-04-14 14:24:35 +0530

    initial commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import "./home.css"
+
+// rgb(246, 51, 102)
+export class home extends Component {
+    constructor(){
+        super()
+        this.state={
+            checked: false
+        };
+    }
+    getInfo = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+          console.log({ res })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+    handleCheckClick = () => {
+        this.setState({ checked: !this.state.checked });
+    }
+    render() {
+        var mytxt = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nisi quasi, possimus enim magnam soluta accusamus beatae temporibus officia error animi dolorum voluptatum iste! Quidem, culpa ipsam! Necessitatibus, voluptatum expedita!"
+        if(this.state.checked === true){
+            console.log("checked")
+        }
+        return (
+            <div>
+                <div className="mainHeading">
+                    <h1>Prediction of Survival in Patients with Heart Failure</h1>
+                </div>
+                <div className="info">
+                    <input type="checkbox" name="check" checked={this.state.checked} onChange={this.handleCheckClick}/>
+                    <span className="checkmark"></span>
+                    <p>Information</p>
+                </div>
+                {this.state.checked ?<p>{mytxt}</p> : ""}
+                <button type="button" onClick={()=>{this.getInfo()}}>Call API</button>
+            </div>
+        )
+    }
+}
+
+export default home
---
+++ src/Components/RootMain.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Home from './Home/Home';
+
+
+export default class RootMain extends Component {
+    render() {
+        return (
+            <Home name= "akshay" />
+        )
+    }
+}
