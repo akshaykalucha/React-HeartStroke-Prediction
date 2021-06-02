c9d6a4daa15b05fc99918de3886d443c0f17100a
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-06-02 22:39:09 +0530

    addming models

---
+++ PyBackend/models.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pandas_profiling as pp
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+import os
+import plotly.graph_objects as go
+import plotly.io as pio
+import pickle
+from sklearn.utils import resample
+# Metrics
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, auc, roc_curve
+
