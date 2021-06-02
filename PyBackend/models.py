da0e3b65fa585e22eac1bddbfdf3e90ec86e2ccc
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-06-02 22:40:05 +0530

    addming models

--- PyBackend/models.py
+++ PyBackend/models.py
@@ -17,3 +17,136 @@ from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.pipeline import Pipeline, make_pipeline
 
 
+# Validation
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.pipeline import Pipeline, make_pipeline
+
+# Tuning
+from sklearn.model_selection import GridSearchCV
+
+# Feature Extraction
+from sklearn.feature_selection import RFE
+
+# Preprocessing
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer, Binarizer, LabelEncoder
+
+# Models
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Ensembles
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+warnings.filterwarnings('ignore')
+
+
+sns.set_style("whitegrid", {'axes.grid' : False})
+pio.templates.default = "plotly_white"
+
+
+
+df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+
+
+# Analyze Data
+def explore_data(df):
+    print("Number of Instances and Attributes:", df.shape)
+    print('\n')
+    print('Dataset columns:',df.columns)
+    print('\n')
+    print('Data types of each columns: ', df.info())
+    
+# Checking for duplicates
+def checking_removing_duplicates(df):
+    count_dups = df.duplicated().sum()
+    print("Number of Duplicates: ", count_dups)
+    if count_dups >= 1:
+        df.drop_duplicates(inplace=True)
+        print('Duplicate values removed!')
+    else:
+        print('No Duplicate values')
+    
+
+# Split training and validation set
+def read_in_and_split_data(data, target):
+    X = data.drop(target, axis=1)
+    y = data[target]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
+    return X_train, X_test, y_train, y_test
+    
+# Spot-Check Algorithms
+def GetModel():
+    Models = []
+    Models.append(('LR'   , LogisticRegression()))
+    Models.append(('LDA'  , LinearDiscriminantAnalysis()))
+    Models.append(('KNN'  , KNeighborsClassifier()))
+    Models.append(('CART' , DecisionTreeClassifier()))
+    Models.append(('NB'   , GaussianNB()))
+    Models.append(('SVM'  , SVC(probability=True)))
+    return Models
+
+def ensemblemodels():
+    ensembles = []
+    ensembles.append(('AB'   , AdaBoostClassifier()))
+    ensembles.append(('GBM'  , GradientBoostingClassifier()))
+    ensembles.append(('RF'   , RandomForestClassifier()))
+    ensembles.append(( 'Bagging' , BaggingClassifier()))
+    ensembles.append(('ET', ExtraTreesClassifier()))
+    return ensembles
+
+# Spot-Check Normalized Models
+def NormalizedModel(nameOfScaler):
+    
+    if nameOfScaler == 'standard':
+        scaler = StandardScaler()
+    elif nameOfScaler =='minmax':
+        scaler = MinMaxScaler()
+    elif nameOfScaler == 'normalizer':
+        scaler = Normalizer()
+    elif nameOfScaler == 'binarizer':
+        scaler = Binarizer()
+
+    pipelines = []
+    pipelines.append((nameOfScaler+'LR'  , Pipeline([('Scaler', scaler),('LR'  , LogisticRegression())])))
+    pipelines.append((nameOfScaler+'LDA' , Pipeline([('Scaler', scaler),('LDA' , LinearDiscriminantAnalysis())])))
+    pipelines.append((nameOfScaler+'KNN' , Pipeline([('Scaler', scaler),('KNN' , KNeighborsClassifier())])))
+    pipelines.append((nameOfScaler+'CART', Pipeline([('Scaler', scaler),('CART', DecisionTreeClassifier())])))
+    pipelines.append((nameOfScaler+'NB'  , Pipeline([('Scaler', scaler),('NB'  , GaussianNB())])))
+    pipelines.append((nameOfScaler+'SVM' , Pipeline([('Scaler', scaler),('SVM' , SVC())])))
+    pipelines.append((nameOfScaler+'AB'  , Pipeline([('Scaler', scaler),('AB'  , AdaBoostClassifier())])  ))
+    pipelines.append((nameOfScaler+'GBM' , Pipeline([('Scaler', scaler),('GMB' , GradientBoostingClassifier())])  ))
+    pipelines.append((nameOfScaler+'RF'  , Pipeline([('Scaler', scaler),('RF'  , RandomForestClassifier())])  ))
+    pipelines.append((nameOfScaler+'ET'  , Pipeline([('Scaler', scaler),('ET'  , ExtraTreesClassifier())])  ))
+
+    return pipelines
+
+# Train model
+def fit_model(X_train, y_train,models):
+    # Test options and evaluation metric
+    num_folds = 10
+    scoring = 'accuracy'
+
+    results = []
+    names = []
+    for name, model in models:
+        kfold = KFold(n_splits=num_folds, random_state=0)
+        cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+        
+    return names, results
+
+# Save trained model
+def save_model(model,filename):
+    pickle.dump(model, open(filename, 'wb'))
+
