783b7725693c001779a8f251bda114b4e3f00129
Akshay <40710016+akshaykalucha3@users.noreply.github.com>
2021-06-02 22:41:02 +0530

    Gradient classifier

--- PyBackend/models.py
+++ PyBackend/models.py
@@ -209,3 +209,91 @@ name,results = fit_model(X_train, y_train, ScaledModel)
 
 # Fine tunong
 
+# Performance Measure
+def classification_metrics(model, conf_matrix):
+    print(f"Training Accuracy Score: {model.score(X_train, y_train) * 100:.1f}%")
+    print(f"Validation Accuracy Score: {model.score(X_test, y_test) * 100:.1f}%")
+    fig,ax = plt.subplots(figsize=(8,6))
+    sns.heatmap(pd.DataFrame(conf_matrix), annot = True, cmap = 'YlGnBu',fmt = 'g')
+    ax.xaxis.set_label_position('top')
+    plt.tight_layout()
+    plt.title('Confusion Matrix', fontsize=20, y=1.1)
+    plt.ylabel('Actual label', fontsize=15)
+    plt.xlabel('Predicted label', fontsize=15)
+    plt.show()
+    print(classification_report(y_test, y_pred))
+    
+# ROC_AUC
+def roc_auc(y_test, y_pred):
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    plt.figure(figsize=(8,6))
+    print(f"roc_auc score: {auc(fpr, tpr)*100:.1f}%")
+    plt.plot(fpr, tpr, color='orange', label='ROC')
+    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+    plt.xlabel('False Positive Rate',fontsize=12)
+    plt.ylabel('True Positive Rate', fontsize=12)
+    plt.title('Receiver Operating Characteristic (ROC) Curve', fontsize=20)
+    plt.legend()
+    plt.show()
+
+
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+IQR = Q3 - Q1
+
+df_out = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+live = df_out[df_out.DEATH_EVENT == 0]
+die = df_out[df_out.DEATH_EVENT==1]
+
+die_upsampled = resample(die,
+                        replace=True,
+                        n_samples=len(live),
+                        random_state=0)
+
+upsampled = pd.concat([live, die_upsampled])
+
+
+# Split data to training and validation set
+target = 'DEATH_EVENT'
+X_train, X_test, y_train, y_test = read_in_and_split_data(upsampled, target)
+
+#Fit data to model
+models = GetModel()
+names,results = fit_model(X_train, y_train,models)
+
+# Normalize data to improve accuracy
+ScaledModel = NormalizedModel('minmax')
+name,results = fit_model(X_train, y_train, ScaledModel)
+
+# Fine tunong
+
+model = GradientBoostingClassifier()
+n_estimators = [10, 100, 1000]
+learning_rate = [0.001, 0.01, 0.1]
+subsample = [0.5, 0.7, 1.0]
+max_depth = [3, 7, 9]
+#define grid search
+
+grid = dict(learning_rate=learning_rate, n_estimators=n_estimators, subsample=subsample, max_depth=max_depth)
+kfold = KFold(n_splits=10, random_state=0)
+cv_results = cross_val_score(model, X_train, y_train, cv=10, scoring='accuracy')
+grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=10, scoring='accuracy',error_score=0)
+grid_result = grid_search.fit(X_train, y_train)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# Predict unseen data
+pipeline = make_pipeline(MinMaxScaler(),  GradientBoostingClassifier(learning_rate=0.1, max_depth=9, n_estimators=1000, subsample=0.7))
+model = pipeline.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+conf_matrix = confusion_matrix(y_test,y_pred)
+classification_metrics(pipeline, conf_matrix)
+roc_auc(y_test, y_pred)
+
+save_model(model, 'model.pkl')
